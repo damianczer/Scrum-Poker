de689251c91ff05444b786f3b5a36494dc31a583
Damian Czerwiński <kontakt@damianczerwinski.pl>
2025-03-18 17:34:25 +0100

    add footer & header components
    created base page compoents

---
+++ application/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">Scrum Poker</div>
+    </header>
+  );
+};
+
+export default Header;
